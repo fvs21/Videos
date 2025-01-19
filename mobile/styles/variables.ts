88e62702ecd1ca94b15d4343f79df155f295bc8d
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-01-18 22:42:35 -0500

    Created mobile

---
+++ mobile/styles/variables.ts
@@ -0,0 +1,33 @@
+export const Colors = {
+    primary300: "#3BCBB7",
+    primary200: "rgba(59, 203, 183, 0.6)",
+    primary100: "rgba(59, 203, 183, 0.25)",
+    secondary300: "#3BCBB7",
+    secondary200: "rgba(50, 109, 101, 0.6)",
+    secondary100: "rgba(50, 109, 101, 0.25)",
+    danger: "#F75555",
+
+    light: {
+        black: "#000000",
+        black100: "#8C8E98",
+        black200: "#666876",
+        black300: "#191d31",
+        background: "#fff",
+        tint: "#0a7ea4",
+        icon: "#687076",
+        tabIconDefault: "#687076",
+        tabIconSelected: "#0a7ea4",
+    },
+
+    dark: {
+        background: "rbg(31, 32, 36)",
+        tint: "#fff",
+        icon: "#9BA1A6",
+        tabIconDefault: "#9BA1A6",
+        tabIconSelected: "#fff",
+        white: "#fff",
+        white100: "#ECEDEE",
+        white200: "#F7F8FA",
+        white300: "#F7F8FA",
+    }
+}
